35367368de81463e50a21eede338169df8198f5f
sreekar <sreekardugyala@my.unt.edu>
2023-03-04 14:50:21 -0600

    first commit

---
+++ src/main/java/com/sreekar/springboot/mywebapp/MywebappApplication.java
@@ -0,0 +1,12 @@
+package com.sreekar.springboot.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebappApplication.class, args);
+	}
+}
